03acc6b221b5b22214f02126dd1635bd4ca9eb21
Christopher Herrington <christopher.herrington@outlook.com>
2019-09-25 13:38:22 -0400

    adds modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 "use strict";
 
 const chuckQuotesForm = document.querySelector('#chuckQuotesForm');
+const closeModal = document.querySelector('.modal__close');
+const getQuotesButton = document.querySelector('#getMoreQuotes');
+
+closeModal.addEventListener('click', (e) => {
+    toggleModal();
+})
 chuckQuotesForm.addEventListener('submit', (e) => {
+    getQuotesButton.classList.add('is-loading');
     e.preventDefault();
     const categoryValue = chuckQuotesForm.querySelector('select').value;
     updateChuckSays(e, categoryValue);
@@ -10,13 +17,19 @@ chuckQuotesForm.addEventListener('submit', (e) => {
 // Add an event listener to the button, DON'T FORGET TO PREVENT THE DEFAULT BEHAVIOR!
 // Call a function to return a new quote, and update the DOM
 
+function toggleModal() {
+    document.querySelector('.wrapper--modal').classList.toggle('open');
+}
+
 // Create a function to update the quote text in the DOM
 function updateChuckSays(ev, category = 'science') {
     const chuckSays = document.getElementById('chuckSays');
-    chuckSays.innerHTML = "Loading..."
+    chuckSays.innerHTML = "loading..."
+    toggleModal()
     get(`https://api.chucknorris.io/jokes/random?category=${category}`,
         (json) => {
             chuckSays.innerHTML = json.value;
+            getQuotesButton.classList.remove('is-loading');
             // img.src = json.icon_url;
         });
 }
